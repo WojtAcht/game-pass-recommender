fd3d40e7de884329e4fd33144764ea4bc0465930
WojtAcht <wachtelik@gmail.com>
2022-11-11 20:48:23 +0100

    Find n random games (with possible duplicates)

--- utils.pl
+++ utils.pl
@@ -33,3 +33,13 @@ random_game(RandomGame) :-
     maplist(divide, RatingsCountList, RepeatedSumOfRatings, Probabilities),
     findall(G, ratings_count(G, RatingsCount), GamesList),
     choice(GamesList, Probabilities, RandomGame).
+
+n_random_games(N, RandomGames) :- 
+    n_random_games(N, [], RandomGames).
+n_random_games(0, CurrentRandomGames, RandomGames) :- 
+    RandomGames = CurrentRandomGames.
+n_random_games(N, CurrentRandomGames, RandomGames) :- 
+    random_game(G),
+    N1 is N-1,
+    append(CurrentRandomGames, [G], NewCurrentRandomGames),
+    n_random_games(N1, NewCurrentRandomGames, RandomGames).
