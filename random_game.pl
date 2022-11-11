4b05e7401636161465e14b2c0ec19412c8fa60bf
WojtAcht <wachtelik@gmail.com>
2022-11-12 00:33:18 +0100

    Add images and names

--- random_game.pl
+++ random_game.pl
@@ -1,11 +1,10 @@
 :- module(random_game,
     [
-        random_game/1,
         n_random_games/2
     ]
 ).
 
-random_game(RandomGame) :-  
+random_game_slug(RandomGame) :-  
     findall(RatingsCount, ratings_count(G, RatingsCount), RatingsCountList),
     sum(RatingsCountList, SumOfRatings),
     length(RatingsCountList, RatingsCountListLength),
@@ -14,12 +13,23 @@ random_game(RandomGame) :-
     findall(G, ratings_count(G, RatingsCount), GamesList),
     choice(GamesList, Probabilities, RandomGame).
 
-n_random_games(N, RandomGames) :- 
-    n_random_games(N, [], RandomGames).
-n_random_games(0, CurrentRandomGames, RandomGames) :- 
-    maplist(term_string, CurrentRandomGames, RandomGames).
-n_random_games(N, CurrentRandomGames, RandomGames) :- 
-    random_game(G),
+n_random_game_slugs(N, RandomGames) :- 
+    n_random_game_slugs(N, [], RandomGames).
+n_random_game_slugs(0, CurrentRandomGames, RandomGames) :- 
+    RandomGames = CurrentRandomGames.
+n_random_game_slugs(N, CurrentRandomGames, RandomGames) :- 
+    random_game_slug(G),
     N1 is N-1,
     append(CurrentRandomGames, [G], NewCurrentRandomGames),
-    n_random_games(N1, NewCurrentRandomGames, RandomGames).
+    n_random_game_slugs(N1, NewCurrentRandomGames, RandomGames).
+
+game(Id, Image, Name, Game) :-
+    Game = game{id: Id, name: Name, image: Image}.
+
+n_random_games(N, RandomGames) :-
+    n_random_game_slugs(N, RandomGameSlugs),
+    maplist(term_string, RandomGameSlugs, RandomGameStrings),
+    maplist(name, RandomGameSlugs, RandomGameNames),
+    maplist(image, RandomGameSlugs, RandomGameImages),
+    maplist(game, RandomGameStrings, RandomGameImages, RandomGameNames, RandomGames).
+    
