65f9ff7eab41b09f582c1529c04d38282d1ac58d
JanTry <jantrynda@gmail.com>
2022-11-20 15:31:49 +0100

    Quick refactor

--- js/src/ButtonsSet.js
+++ js/src/ButtonsSet.js
@@ -3,28 +3,26 @@ import React, { useState } from "react";
 
 
 export const ButtonsSet = (props) => {
-    const [active_yes, setActiveYes] = useState(false); 
-    const [active_neutral, setActiveNeutral] = useState(false); 
-    const [active_no, setActiveNo] = useState(false); 
+    const [active, setActive] = useState([false, false, false]); 
 
     const YesCallback = () => {
-        setActiveYes(!active_yes);
+        setActive([!active[0], active[1], active[2]]);
         props.callback(props.gameIndex, 1);
     }
     const NeutralCallback = () => {
-        setActiveNeutral(!active_neutral);
+        setActive([active[0], !active[1], active[2]]);
         props.callback(props.gameIndex, 0);
     }
     const NoCallback = () => {
-        setActiveNo(!active_no);
+        setActive([active[0], active[1], !active[2]]);
         props.callback(props.gameIndex, -1);
     }
     
     return (
       <div className="my-button">
-        <Button type="button" value="1" disabled={active_yes || active_neutral || active_no} variant={ active_yes ? "success" : "outline-success" } onClick={() => YesCallback()} > Like </Button>{' '}
-        <Button type="button" value="0" disabled={active_yes || active_neutral || active_no} variant={ active_neutral ? "dark" : "outline-dark" } onClick={() => NeutralCallback()} > Neutral </Button>{' '}
-        <Button type="button" value="-1" disabled={active_yes || active_neutral || active_no} variant={ active_no ? "danger" : "outline-danger" } onClick={() => NoCallback()} > Dislike </Button>{' '}
+        <Button type="button" value="1" disabled={active[0] || active[1] || active[2]} variant={ active[0] ? "success" : "outline-success" } onClick={() => YesCallback()} > Like </Button>{' '}
+        <Button type="button" value="0" disabled={active[0] || active[1] || active[2]} variant={ active[1] ? "dark" : "outline-dark" } onClick={() => NeutralCallback()} > Neutral </Button>{' '}
+        <Button type="button" value="-1" disabled={active[0] || active[1] || active[2]} variant={ active[2] ? "danger" : "outline-danger" } onClick={() => NoCallback()} > Dislike </Button>{' '}
       </div>
     );
 };
